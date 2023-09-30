0a73c668c996028ea6c8fd768eb2932af4e638ac
ithivesolutions <124482413+ithivesolutions@users.noreply.github.com>
2023-09-30 23:37:41 +1000

    fix: minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -45,7 +45,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Let's connect on LinkedIn!
+            Let&apos;s connect on LinkedIn!
           </p>
         </a>
 
