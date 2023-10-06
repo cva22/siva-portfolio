75af48eaf1db67832f09b8bcbb58c37668dd9dd6
ithivesolutions <124482413+ithivesolutions@users.noreply.github.com>
2023-10-06 21:52:58 +1100

    fix: readme and github link fixed

--- src/app/components/hyperLinks.tsx
+++ src/app/components/hyperLinks.tsx
@@ -19,7 +19,7 @@ export default function HyperLinks() {
       </a>
 
       <a
-        href='https://github.com/sivaish; https://github.com/ithivesolutions'
+        href='https://github.com/sivaish'
         className='group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30'
         target='_blank'
         rel='noopener noreferrer'
