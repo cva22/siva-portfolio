7097da1b4a5aa9be2cfc8a6d6e2dd3922249ce58
ithivesolutions <124482413+ithivesolutions@users.noreply.github.com>
2023-10-22 21:14:08 +1100

    chore: under development fix

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,9 +2,11 @@ import { Caption } from '../ui/typography';
 
 export default function Footer() {
   return (
-    <div className='relative z-10 w-full bg-neutrals-900 py-32 flex flex-col items-center justify-center'>
+    <div className='relative z-10 w-full bg-neutrals-900 py-32 flex flex-col items-center text-center justify-center'>
       <Caption className='border-secondary/30 bg-secondary/10 text-secondary after:bg-secondary/10' id='inprogress-caption'>
-        Currently viewing the Alpha version ~ Site under development
+        Currently viewing the Alpha version
+        <br />
+        ~ Site under development ~
       </Caption>
     </div>
   );
